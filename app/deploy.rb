6f676b78a0ea223823216be6a358b1c8fc674cc4
Simonas Šerlinskas <simonas.serlinskas@nfq.com>
2017-04-07 09:38:33 +0300

    deploy info

--- app/deploy.rb
+++ app/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'akademija'
 set :repo_url, '#'
 
-set :deploy_to, '/home/kickstart/'
+set :deploy_to, '/home/mad/'
 
 set :archive_cache, true
 set :branch, 'master'
@@ -9,4 +9,4 @@ set :scm, :rsync
 set :exclude, ['.git', 'node_modules', 'web/app_dev.php']
 
 set :linked_files, fetch(:linked_files, []).push('app/config/parameters.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('app/logs')
+set :linked_dirs, fetch(:linked_dirs, []).push('var/logs')
