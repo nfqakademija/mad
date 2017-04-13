6d71f3c6a27b2744c817ca5fe7a8d7cd930798df
Aistis Cekanauskis <aistis.cekanauskiss@gmail.com>
2017-04-13 17:06:09 +0300

    Added RegistrationType.php for FosUserBundle

---
+++ src/AppBundle/Form/RegistrationType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+        // Or for Symfony < 2.8
+        // return 'fos_user_registration';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'appbundle_user_registration';
+    }
+}
