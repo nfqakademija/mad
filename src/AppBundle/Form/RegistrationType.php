de6c672621e0f77ea09885a582f3ac244f16fade
Dominick Seputis <dom.seputis@gmail.com>
2017-04-14 15:19:33 +0300

    added custom login

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -5,26 +5,21 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('email');
         $builder->add('fullname');
         $builder->add('weight');
         $builder->add('height');
         $builder->add('activity');
-        $builder->add('b_food');
+
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function getParent()
     {
-        $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\User',
-            'csrf_token_id' => 'registration',
-        ));
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
     }
 
     public function getBlockPrefix()
