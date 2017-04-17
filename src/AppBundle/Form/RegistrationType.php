553b084574a301bfd0b7fb5a95eac46925ba0efa
Dominick Seputis <dom.seputis@gmail.com>
2017-04-17 23:11:37 +0300

    Completed registration

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -4,17 +4,40 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('fullname');
-        $builder->add('weight');
-        $builder->add('height');
-        $builder->add('activity');
-        $builder->add('b_food');
+        $builder->add('name', TextType::class, array(
+            'label' => 'Vardas'
+        ));
+        $builder->add('lname', TextType::class, array(
+            'label' => 'Pavardė'
+        ));
+        $builder->add('weight', TextType::class, array(
+            'label' => 'Svoris', 'attr' => array(
+                'placeholder' => 'kg')
+        ));
+        $builder->add('height', TextType::class, array(
+            'label' => 'Ūgis', 'attr' => array(
+                'placeholder' => 'cm')
+        ));
+        $builder->add('activity', ChoiceType::class, array(
+        'choices'  => array(
+            'Pasyvus 0 val.' => 1,
+            'Lengvai aktyvus 1-2 val.' => 2,
+            'Vidutiniškai aktyvus 2-3 val.' => 3,
+            'Labai aktyvus 3-5 val.' => 3,
+            'Ypač aktyvus 5+ val.' => 4,
+        ), 'choices_as_values' => true,'multiple'=>false,'expanded'=>true,
+        'label' => 'Fizinis aktyvumas per savaitę',
+         ));
+        $builder->remove('b_food');
+        $builder->remove('username');
 
     }
 
