ed0d5b902ed4a5e208797cbcd0066b7a1e5c8112
Aistis Cekanauskis <aistis.cekanauskiss@gmail.com>
2017-05-24 22:15:24 +0300

    Nepavyko

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
+            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
 
             new AppBundle\AppBundle(),
         );
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,118 +3,31 @@
 
 namespace AppBundle\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use FOS\UserBundle\Model\User as BaseUser;
+use FOS\UserBundle\Model\User as FOSUBUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ORM\Table(name="users")
  * @ORM\Entity
- * @ORM\Table(name="fos_user")
  */
-class User extends BaseUser
+class User extends FOSUBUser
 {
     /**
+     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
-     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
     /**
-     * @ORM\OneToMany(targetEntity="AppBundle\Entity\UserMealsSchedules", mappedBy="user")
+     * @ORM\Column(name="facebook_id", type="string", length=255, nullable=true)
      */
-    private $mealsSchedules;
+    private $facebookId;
+
+    private $facebookAccessToken;
 
     /**
-     * @ORM\OneToMany(targetEntity="AppBundle\Entity\UserBlockedIngredients", mappedBy="userId")
-     */
-    private $blockedIngredients;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    protected $name;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    protected $lname;
-
-    /**
-     * @return mixed
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param mixed $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getLname()
-    {
-        return $this->lname;
-    }
-
-    /**
-     * @param mixed $lname
-     */
-    public function setLname($lname)
-    {
-        $this->lname = $lname;
-    }
-
-    /**
-     * @ORM\Column(type="integer", length=3)
-     */
-    protected $weight;
-
-    /**
-     * @ORM\Column(type="integer", length=3)
-     */
-    protected $height;
-
-    /**
-     * @ORM\Column(type="integer", length=1)
-     */
-    protected $activity;
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->mealsSchedules = new ArrayCollection();
-        $this->blockedIngredients = new ArrayCollection();
-        // your own logic
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getActivity()
-    {
-        return $this->activity;
-    }
-
-    /**
-     * @param mixed $activity
-     */
-    public function setActivity($activity)
-    {
-        $this->activity = $activity;
-    }
-
-
-
-    /**
-     * @return mixed
+     * @return integer
      */
     public function getId()
     {
@@ -122,51 +35,40 @@ class User extends BaseUser
     }
 
     /**
-     * @param mixed $id
+     * @param string $facebookId
+     * @return User
      */
-    public function setId($id)
+    public function setFacebookId($facebookId)
     {
-        $this->id = $id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getWeight()
-    {
-        return $this->weight;
-    }
-
-    /**
-     * @param mixed $weight
-     */
-    public function setWeight($weight)
-    {
-        $this->weight = $weight;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getHeight()
-    {
-        return $this->height;
-    }
-
-    /**
-     * @param mixed $height
-     */
-    public function setHeight($height)
-    {
-        $this->height = $height;
-    }
-
-    public function setEmail($email)
-    {
-        $email = is_null($email) ? '' : $email;
-        parent::setEmail($email);
-        $this->setUsername($email);
+        $this->facebookId = $facebookId;
 
         return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFacebookId()
+    {
+        return $this->facebookId;
+    }
+
+    /**
+     * @param string $facebookAccessToken
+     * @return User
+     */
+    public function setFacebookAccessToken($facebookAccessToken)
+    {
+        $this->facebookAccessToken = $facebookAccessToken;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFacebookAccessToken()
+    {
+        return $this->facebookAccessToken;
     }
 }
